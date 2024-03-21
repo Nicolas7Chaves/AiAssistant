8e2a800e56e5b81a43a8bd94908433ffbb5e7ab3
Nicolas7Chaves <nicolas7chaves@gmail.com>
2024-03-21 11:15:31 -0400

    remove allow openbrowser config to openai

--- backend/config.js
+++ backend/config.js
@@ -4,6 +4,5 @@ import { createClient } from "@supabase/supabase-js";
 /** OpenAI config */
 if (!process.env.OPENAI_API_KEY) throw new Error("OpenAI API key is missing or invalid.");
 export const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
-    dangerouslyAllowBrowser: true
+    apiKey: process.env.OPENAI_API_KEY
 });
