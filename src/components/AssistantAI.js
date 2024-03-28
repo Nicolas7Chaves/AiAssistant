7624b382040596c3f709d84a023d7ff6c89f7c9a
Nicolas7Chaves <nicolas7chaves@gmail.com>
2024-03-28 15:36:13 -0400

    add consts for sending axios post request with data for prompt

--- src/components/AssistantAI.js
+++ src/components/AssistantAI.js
@@ -1,16 +1,38 @@
+import { useState } from 'react';
+import './styles.scss'
+import axios from 'axios';
 
 function AssistantAI() {
+    const [userInput, setUserInput] = useState('');
+    
+    const handleInputChange = (e) => {
+        setUserInput(e.target.value);
+    }
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post("/create-message",{content: userInput});
+            console.log(response.data)
+        } catch (error) {
+            console.error("Failed to send:", error)
+        }
+    }
 
     return (
         <>
-            <h1>
+            <h1 className='form__title'>
                 MovieRecs
             </h1>
-            <form>
-                <input type='text' placeholder='How can I help you?' />
-                    <button type='submit' className='form__btn'>
-                        Send
-                    </button>
+            <form className='form' onSubmit={handleSubmit}>
+                <input
+                    className='form__input' type='text' placeholder='How can I help you?'
+                    value={userInput}
+                    onChange={handleInputChange}
+                />
+                <button type='submit' className='form__btn'>
+                    Send
+                </button>
             </form>
         </>
     );
